3818bc661212434d87331a61642cbae3fb10f39a
ndevasia <ndevasia@mit.edu>
2020-04-30 00:43:05 -0400

    quiz prototype working

--- website-demo/_site/main.js
+++ website-demo/_site/main.js
@@ -3,15 +3,19 @@ function check(){
   var question1 = document.quiz.question1.value;
   var question2 = document.quiz.question2.value;
   var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
   var correct = 0
 
-  if (question1 == "Providence"){
+  if (question1.toLowerCase() == "generative adversarial network"){
     correct++;
   }
-  if (question2 == "Hartford"){
+  if (question2 == "2014"){
     correct++;
   }
-  if (question3 == "Albany"){
+  if (question3 == "Neural networks classify and GANs generate."){
+    correct++;
+  }
+  if (question4 == "livingroom"){
     correct++;
   }
   document.getElementById("after_submit").style.visibility = "visible";
